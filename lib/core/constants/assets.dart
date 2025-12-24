39c3d2505513b4c2508d24fc751f377cd6693d0d
Luri Darmawan <luri@carik.id>
2025-12-24 12:19:02 +0700

    add logo assets

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -3,19 +3,20 @@ class Assets {
   Assets._();
 
   // Logo
-  static const String logo = 'assets/images/logo.png';
-  static const String logoWhite = 'assets/images/logo_white.png';
+  // static const String logo = 'assets/images/logo.png';
+  static const String logo = 'assets/images/logo/carik_blue_logo.png';
+  static const String logoWhite = 'assets/images/logo/carik_green_Logo.png.png';
   
   // Splash Screen
-  static const String splashImage = 'assets/images/splash.png';
-  static const String splashBackground = 'assets/images/splash_bg.png';
+  // static const String splashImage = 'assets/images/splash.png';
+  // static const String splashBackground = 'assets/images/splash_bg.png';
   
   // Placeholder
-  static const String placeholder = 'assets/images/placeholder.png';
-  static const String userPlaceholder = 'assets/images/user_placeholder.png';
+  // static const String placeholder = 'assets/images/placeholder.png';
+  // static const String userPlaceholder = 'assets/images/user_placeholder.png';
   
   // Remote URLs (default fallback)
   static const String remoteSplashUrl = 'https://picsum.photos/800/1600';
-  static const String remoteLogoUrl = 'https://picsum.photos/200';
-  static const String remoteBannerUrl = 'https://picsum.photos/800/400';
+  // static const String remoteLogoUrl = 'https://picsum.photos/200';
+  // static const String remoteBannerUrl = 'https://picsum.photos/800/400';
 }
