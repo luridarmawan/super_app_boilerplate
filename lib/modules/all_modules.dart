557977d7f1ff5bf3459a8b41356faeadbf3b8dac
Luri Darmawan <luri@carik.id>
2025-12-31 09:52:16 +0700

    update all_modules

--- lib/modules/all_modules.dart
+++ lib/modules/all_modules.dart
@@ -1,7 +1,4 @@
 import 'module_registry.dart';
-// import 'inventory/inventory_module.dart';
-// import 'invoice/invoice_module.dart';
-// import 'sales/sales_module.dart';
 import 'sample/demo_module.dart';
 import 'sample/sample_module.dart';
 
@@ -9,9 +6,6 @@ import 'sample/sample_module.dart';
 /// This file registers all available modules to the registry.
 class ModuleManifest {
   static void register() {
-    // ModuleRegistry.register(InventoryModule());
-    // ModuleRegistry.register(InvoiceModule());
-    // ModuleRegistry.register(SalesModule());
     ModuleRegistry.register(DemoModule());
     ModuleRegistry.register(SampleModule());
   }
