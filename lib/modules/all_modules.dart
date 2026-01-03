5c5815ead42fce08cdea5bf7a17f5eef1b8a3af0
Luri Darmawan <luri@carik.id>
2026-01-03 21:30:13 +0700

    arcticle recommendation widget

--- lib/modules/all_modules.dart
+++ lib/modules/all_modules.dart
@@ -1,9 +1,7 @@
 import 'module_registry.dart';
+import 'news/news_module.dart';
 import 'sample/demo_module.dart';
 import 'sample/sample_module.dart';
-// import 'package:super_module/super_module_module.dart';
-// import 'package:crm/crm_module.dart';
-import 'package:arrow_sense/arrow_sense_module.dart';
 
 /// Auto-generated file. Do not edit manually.
 /// This file registers all available modules to the registry.
@@ -11,8 +9,6 @@ class ModuleManifest {
   static void register() {
     ModuleRegistry.register(DemoModule());
     ModuleRegistry.register(SampleModule());
-    // ModuleRegistry.register(SuperModuleModule());
-    // ModuleRegistry.register(CrmModule());
-    ModuleRegistry.register(ArrowSenseModule());
+    ModuleRegistry.register(NewsModule());
   }
 }
